a18e91451bc4f691d1fd6dab46dd11984030dfd2
londyy123 <londynnista1128@gmail.com>
2025-02-11 17:40:16 -0500

    Update WatchMovie.java

--- src/main/java/com/codedifferently/labs/partA/ex01/WatchMovie.java
+++ src/main/java/com/codedifferently/labs/partA/ex01/WatchMovie.java
@@ -4,7 +4,15 @@ public class WatchMovie {
     public static String watchMovie() {
         String response ="";
         /* Your code goes here*/
+        int moviePrice = 12;
+        int rating = 5;
 
+        if (moviePrice >= 12) {
+            System.out.println("I am interested in the movie.");
+        }
+        else {
+            System.out.println("I am not interested in the movie.");
+        }
 
         response += ("Enter the movie ticket price \n");
         return response;
