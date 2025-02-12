503b47f4d95323976f90870fbc141745f2401ac6
londyy123 <londynnista1128@gmail.com>
2025-02-12 18:14:07 -0500

    Update ComputeFare.java

--- src/main/java/com/codedifferently/labs/partA/ex03/ComputeFare.java
+++ src/main/java/com/codedifferently/labs/partA/ex03/ComputeFare.java
@@ -6,7 +6,17 @@ public class ComputeFare {
     public static int compute (int age){
         int response = 0;
         /* Put your code in between these comments : Top */
-
+        int userAge;
+        int fare;
+        if (userAge < 11) {
+            fare = 3;
+        }
+        else if (userAge > 11 && userAge < 65) {
+            fare = 5;
+        }
+        else {
+            fare = 3;
+        }
 
         /* Put your code in between these comments : Bottom */
 
